d3bc687592ba7b4f62ffdb1c9d47247d3754ce96
Brett <codex.estates@gmail.com>
2023-12-29 16:15:10 -0500

    complete-ci-build

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,8 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """ List accounts """
@@ -84,6 +86,7 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """Reads one account"""
@@ -93,7 +96,7 @@ def get_accounts(account_id):
 
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
     if not find_account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account was not found.")
+        abort(status.HTTP_404_NOT_FOUND, "Account was not found.")
 
     # return the serialize() version of the account with a return code of status.HTTP_200_OK
     return find_account.serialize(), status.HTTP_200_OK
@@ -103,6 +106,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -121,7 +125,7 @@ def update_accounts(account_id):
     # call account.update() to update the account with the new data
     find_account.update()
 
-    # return the serialize() version of the account with a return code of status.HTTP_200_OK    
+    # return the serialize() version of the account with a return code of status.HTTP_200_OK
     return find_account.serialize(), status.HTTP_200_OK
 
 ######################################################################
@@ -129,6 +133,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["Delete"])
 def delete_accounts(account_id):

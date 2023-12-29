4e7110df04ac874cd13c3c827ef32e156c0a3470
Brett <codex.estates@gmail.com>
2023-12-29 13:12:03 -0500

    update-account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,3 +171,37 @@ class TestAccountService(TestCase):
 
         # assert that the len() of the data is 5 (the number of accounts you created)
         self.assertEqual(len(data_response), 5)
+
+
+    """Update Accounts"""
+
+    def test_update_account(self):
+        """Should update an account"""
+
+        # create an Account to update
+        test_account = AccountFactory()
+
+        # send a self.client.post() request to the BASE_URL with a json payload of test_account.serialize()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        
+        # assert that the resp.status_code is status.HTTP_201_CREATED
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        # update the account
+        # get the data from resp.get_json() as new_account
+        new_account = response.get_json()
+        
+        # change new_account["name"] to Super Sonic
+        new_account["name"] = "Super Sonic"
+        
+        # send a self.client.put() request to the BASE_URL with a json payload of new_account
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+    
+        # assert that the resp.status_code is status.HTTP_200_OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # get the data from resp.get_json() as updated_account
+        updated_account = response.get_json()
+        
+        # assert that the updated_account["name"] is equal to Super Sonic
+        self.assertEqual(updated_account["name"], "Super Sonic")
